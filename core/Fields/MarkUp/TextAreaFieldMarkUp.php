f9db4131b9c21919ce4b6e3a9836d48d6ccada02
Chance Nyasulu <nyasuluchance6@gmail.com>
2024-05-30 07:50:56 +0530

    theme

--- core/Fields/MarkUp/TextAreaFieldMarkUp.php
+++ core/Fields/MarkUp/TextAreaFieldMarkUp.php
@@ -16,13 +16,20 @@ class TextAreaFieldMarkUp implements FieldMarkUpInterface
     public function buildMarkup(FieldInterface $field, array|null $default_value): FieldMarkUpInterface
     {
         $default_value = $default_value['value'] ?? null;
-       $this->field = $field;
+        $this->field = $field;
+        $display_setting = $field->getDisplayType()['name'] ?? 'plain_text';
+        $class_name = match ($display_setting) {
+            'plain_text' => null,
+            'full_html' => 'tinymce-editor',
+             default => 'quill-editor',
+        };
+
         $is_required = !empty($field->isRequired()) ? 'required' : null;
         $size = $this->field->getSize();
         $this->markup = <<<FIELD_MARKUP
                <div class="form-group field-markup mt-3">
                <label for="field-{$this->field->getName()}">{$this->field->getLabel()}</label>
-               <textarea name="{$this->field->getName()}" id="field-{$this->field->getName()}" class="form-control input-field-text"
+               <textarea name="{$this->field->getName()}" id="field-{$this->field->getName()}" class="form-control input-field-text $class_name"
                 $is_required size="$size">{$default_value}</textarea>
                </div>
 FIELD_MARKUP;
